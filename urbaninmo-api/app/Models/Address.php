89f198a1cd454e266572a568b68c072e732bb4bb
Luis Duarte <ladh0907@gmail.com>
2024-10-13 18:54:00 -0600

    added Address and Comments -msf

---
+++ urbaninmo-api/app/Models/Address.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+     
+    use HasFactory;
+    protected $table = 'addresses';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+
+    protected $fillable = [
+        'address',
+        'zipcode',
+        'city',
+        'state',
+        'country'
+    ];
+}
---
+++ urbaninmo-api/database/seeders/AddressSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Address;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AddressSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Address::create([
+            'address' => 'Calle 1 #123',
+            'zipcode' => '12345',
+            'city' => 'Ciudad de México',
+            'state' => 'CDMX',
+            'country' => 'México'
+        ]);
+    }
+}
