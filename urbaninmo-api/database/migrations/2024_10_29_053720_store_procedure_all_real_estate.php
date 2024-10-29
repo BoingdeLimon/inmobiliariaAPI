6c43b85455dbbd473df5a1535df120c6b492c5bf
Luis Duarte <ladh0907@gmail.com>
2024-10-29 17:54:19 -0600

    feat 'migracion de store procedure'

---
+++ urbaninmo-api/database/migrations/2024_10_29_053720_store_procedure_all_real_estate.php
@@ -0,0 +1,91 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('
+        CREATE OR REPLACE FUNCTION get_all_realestate_details()
+        RETURNS TABLE(
+            id BIGINT,
+            title VARCHAR,
+            description TEXT,
+            size DOUBLE PRECISION,
+            rooms INT,
+            bathrooms INT,
+            type VARCHAR,
+            garage BOOLEAN,
+            garden BOOLEAN,
+            patio BOOLEAN,
+            price NUMERIC,
+            isOccupied BOOLEAN,
+            pdf VARCHAR,
+            address VARCHAR,
+            zipcode VARCHAR,
+            city VARCHAR,
+            state VARCHAR,
+            country VARCHAR,
+            x NUMERIC,
+            y NUMERIC,
+            photos VARCHAR[],
+            comments TEXT[]
+        ) AS $$
+        BEGIN
+            RETURN QUERY
+            SELECT 
+            re.id,
+            re.title,
+            re.description,
+            re.size,
+            re.rooms,
+            re.bathrooms,
+            re.type,
+            re.has_garage,
+            re.has_garden,
+            re.has_patio,
+            re.price,
+            re.is_occupied,
+            re.pdf,
+            a.address,
+            a.zipcode,
+            a.city,
+            a.state,
+            a.country,
+            a.x,
+            a.y,
+            ARRAY_AGG(DISTINCT p.photo) AS photos,
+            ARRAY_AGG(DISTINCT c.comment) AS comments
+        FROM 
+            real_estate re
+        LEFT JOIN 
+            address a ON re.id_address = a.id
+        LEFT JOIN 
+            photos p ON p.id_propiedad = re.id
+        LEFT JOIN 
+            comments c ON c.id_property = re.id
+        GROUP BY 
+            re.id, a.id;
+        END;
+        $$ LANGUAGE plpgsql;
+    ');
+    
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP FUNCTION IF EXISTS get_all_realestate_details');
+    }
+};
