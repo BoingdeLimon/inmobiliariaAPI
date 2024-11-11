d41f51e4c5e6da4056f4714b4ccbb72c36d8b355
Luis Duarte <ladh0907@gmail.com>
2024-11-11 00:08:28 -0600

    feat: 'Cambios a RentalCard'

--- urbaninmo-api/app/Http/Controllers/RealEstateController.php
+++ urbaninmo-api/app/Http/Controllers/RealEstateController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Auth;
 class RealEstateController extends Controller
 {
 
+    
     public function index()
     {
         return response()->json([
--- urbaninmo-api/app/Livewire/Rentals.php
+++ urbaninmo-api/app/Livewire/Rentals.php
@@ -2,76 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\RealEstate;
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class Rentals extends Component
 {
     public $rentals = [];
+
     public function mount()
     {
-        $this->rentals = [
-            [
-                'id' => 1,
-                'price' => '20,000 MXN',
-                'location' => 'Club de Golf Tres Marías',
-                'size' => '150 m²',
-                'rooms' => '3 Habitaciones',
-                'bathrooms' => '2 Baños',
-                'amenities' => ['WiFi', 'Parking', 'Pool'],
-                'image' => 'https=>//img10.naventcdn.com/avisos/resize/18/01/41/56/09/41/1200x1200/1125611697.jpg?isFirstImage=true',
-            ],
-            [
-                'id' => 2,
-                'price' => '15,000 MXN',
-                'location' => 'Lomas de Angelópolis',
-                'size' => '120 m²',
-                'rooms' => '2 Habitaciones',
-                'bathrooms' => '2 Baños',
-                'amenities' => ['WiFi', 'Gym'],
-                'image' => 'https://img10.naventcdn.com/avisos/resize/18/01/41/56/09/41/1200x1200/1125611697.jpg?isFirstImage=true',
-            ],
-            [
-                'id' => 3,
-                'price' => '20,000 MXN',
-                'location' => 'Club de Golf Tres Marías',
-                'size' => '150 m²',
-                'rooms' => '3 Habitaciones',
-                'bathrooms' => '2 Baños',
-                'amenities' => ['WiFi', 'Parking', 'Pool'],
-                'image' => 'https=>//img10.naventcdn.com/avisos/resize/18/01/41/56/09/41/1200x1200/1125611697.jpg?isFirstImage=true',
-            ],
-            [
-                'id' => 4,
-                'price' => '15,000 MXN',
-                'location' => 'Lomas de Angelópolis',
-                'size' => '120 m²',
-                'rooms' => '2 Habitaciones',
-                'bathrooms' => '2 Baños',
-                'amenities' => ['WiFi', 'Gym'],
-                'image' => 'https://img10.naventcdn.com/avisos/resize/18/01/41/56/09/41/1200x1200/1125611697.jpg?isFirstImage=true',
-            ],
-            [
-                'id' => 5,
-                'price' => '20,000 MXN',
-                'location' => 'Club de Golf Tres Marías',
-                'size' => '150 m²',
-                'rooms' => '3 Habitaciones',
-                'bathrooms' => '2 Baños',
-                'amenities' => ['WiFi', 'Parking', 'Pool'],
-                'image' => 'https=>//img10.naventcdn.com/avisos/resize/18/01/41/56/09/41/1200x1200/1125611697.jpg?isFirstImage=true',
-            ],
-            [
-                'id' => 6,
-                'price' => '15,000 MXN',
-                'location' => 'Lomas de Angelópolis',
-                'size' => '120 m²',
-                'rooms' => '2 Habitaciones',
-                'bathrooms' => '2 Baños',
-                'amenities' => ['WiFi', 'Gym'],
-                'image' => 'https://img10.naventcdn.com/avisos/resize/18/01/41/56/09/41/1200x1200/1125611697.jpg?isFirstImage=true',
-            ],
-        ];
+        $this->rentals = RealEstate::with(['photos', 'address'])->get()->toArray(); 
     }
+
     public function render()
     {
         return view('livewire.rentals');
