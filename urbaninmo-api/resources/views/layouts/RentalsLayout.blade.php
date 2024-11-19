d3146dd3d11f3b288ae831427ef9f063d2e593f3
Luis Duarte <ladh0907@gmail.com>
2024-11-18 18:34:49 -0600

    feat:xd

--- urbaninmo-api/resources/views/layouts/RentalsLayout.blade.php
+++ urbaninmo-api/resources/views/layouts/RentalsLayout.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>UrbanInmo</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" /> 
+    {{-- <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />  --}}
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"> 
+
 </head>
 <body  class="{{ session('darkMode') ? 'dark' : '' }} ">
     <x-header /> 
