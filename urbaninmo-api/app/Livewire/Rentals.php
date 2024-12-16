12920e32c345464c1dc75e8a2ed49a5217f1c0a1
Luis Duarte <ladh0907@gmail.com>
2024-12-16 13:35:17 -0600

    feat: enhance rental pagination and loading state management

--- urbaninmo-api/app/Livewire/Rentals.php
+++ urbaninmo-api/app/Livewire/Rentals.php
@@ -8,9 +8,12 @@ use Livewire\Component;
 class Rentals extends Component
 {
     public $rentals = [];
+    public $filteredRentals = []; // Almacena todas las propiedades recibidas
     public $currentPage = 1;
-    public $perPage =2 ; // Número de elementos por página
+    public $perPage = 4; 
     public $totalPages;
+    public $isLoading = false;
+    protected $listeners = ['filtersApplied' => 'updateRealEstates'];
 
     public function mount()
     {
@@ -19,6 +22,7 @@ class Rentals extends Component
 
     public function loadRentals()
     {
+        $this->isLoading = true;
         $totalRentals = RealEstate::count();
         $this->totalPages = ceil($totalRentals / $this->perPage);
         $this->rentals = RealEstate::with(['address', 'photos'])
@@ -26,14 +30,32 @@ class Rentals extends Component
             ->limit($this->perPage)
             ->get()
             ->toArray();
+        $this->isLoading = false;
     }
 
     public function goToPage($page)
     {
         if ($page > 0 && $page <= $this->totalPages) {
             $this->currentPage = $page;
-            $this->loadRentals();
+            $this->paginateFilteredRentals();
         }
+    }
+
+    public function updateRealEstates($realEstates)
+    {
+        $this->isLoading = true;
+        $this->filteredRentals = $realEstates; // Almacena todas las propiedades filtradas
+        $this->totalPages = ceil(count($this->filteredRentals) / $this->perPage); // Calcula las páginas totales
+        $this->currentPage = 1; // Reinicia la paginación a la primera página
+        $this->paginateFilteredRentals(); // Pagina las propiedades recibidas
+        $this->isLoading = false;
+    }
+
+    private function paginateFilteredRentals()
+    {
+        // Divide las propiedades filtradas según la página actual y el tamaño de página
+        $offset = ($this->currentPage - 1) * $this->perPage;
+        $this->rentals = array_slice($this->filteredRentals, $offset, $this->perPage);
     }
 
     public function render()
