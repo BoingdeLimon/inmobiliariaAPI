5c5b5c35a56eaa09139774174b44bf5812b57ef1
Luis Duarte <ladh0907@gmail.com>
2024-12-16 19:33:01 -0600

    fix: arreglo lista rentals

--- urbaninmo-api/resources/views/livewire/rental-list.blade.php
+++ urbaninmo-api/resources/views/livewire/rental-list.blade.php
@@ -1,15 +1,17 @@
 <div>
-    <div class="md:ml-64 sm:ml-auto max-w-screen-lg px-4 py-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="md:ml-auto sm:ml-auto  px-4 py-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         @foreach ($rentals as $rental)
             <div
                 class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <img src="{{ $rental->photos->first()
-                    ? asset('storage/photos/' . $rental->photos->first()->photo)
-                    : asset('img/defaultRental.jpg') }}"
-                    alt="{{ $rental->title }} Photo" class="rounded-t-lg h-56 w-full object-cover">
+                <div class="max-w-sm max-h-sm">
+                    <img src="{{ $rental->photos->first()
+                        ? asset('storage/photos/' . $rental->photos->first()->photo)
+                        : asset('img/defaultRental.jpg') }}"
+                        alt="{{ $rental->title }} Photo" class="rounded-t-lg w-full h-56 object-cover">
+                </div>
                 <div class="p-5">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        {{ $rental->title }}</h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $rental->title }}
+                    </h5>
                     <h3 class="mb-2 text-lg text-gray-900 dark:text-white">{{ $rental->type }}</h3>
                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $rental->description }}</p>
                     <p class="text-sm text-gray-600 dark:text-gray-400">Size: {{ $rental->size }} m²</p>
@@ -19,11 +21,17 @@
                     </p>
                     <p class="text-sm text-gray-600 dark:text-gray-400">Location: {{ $rental->address->address }},
                         {{ $rental->address->city }}, {{ $rental->address->state }}</p>
-                    @livewire('new-rentals-form', ['user_id' => null, 'realEstateId' => $rental->id], key($rental->id))
-                    @livewire('confirmar-borrar-modal-real-estate', ['rental' => $rental])
-
+                    <div class="flex mt-4 flex-row justify-evenly">
+                        <button
+                            class=" text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900">
+                            Editar
+                        </button>
+                        @livewire('confirmar-borrar-modal-real-estate', ['rental' => $rental])
+                    </div>
                 </div>
             </div>
         @endforeach
+
+
     </div>
 </div>
